31ab1e97a34892c7799cbdb436cc6884f1f58e16
Heli <hl751966@dal.ca>
2022-01-24 20:52:00 -0400

    added test aoi call

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'careu';
+  constructor(private httpClient: HttpClient) {
+  }
+  ngOnInit() {
+    this.httpClient.get(`https://sample-node-serve.herokuapp.com/api/hello`).pipe(
+      map((response: any) => {
+        debugger;
+        return response;
+      })
+    );
+  }
 }
